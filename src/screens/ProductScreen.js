fe797d8fc62e3607af72ae2354d6a5a76cdf2c2e
saifulalamfahime <you@example.com>
2022-03-29 15:36:47 +0600

    made ui design for product screen page

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { getProduct } from "../api";
 import { parseRequestUrl } from "../utils";
+import Rating from "../components/Rating"
 
    
    const ProductScreen =  {
@@ -9,7 +10,54 @@ import { parseRequestUrl } from "../utils";
           if (product.error) {
               return`<div>${product.error}</div>`;
           }
-          return `<h1>${product.name}</h1>`;
+          return ` <div class="container">
+          <div class="back to result">
+              <a href="/#/">Back to result</a>
+          </div>
+          <div class="details">
+              <div class="details-image">
+                  <img src="${product.image}" alt="${product.name}">
+              </div>
+              <div class="details-info">
+                  <ul>
+                      <li>
+                          <h1>${product.name}</h1>
+                      </li>
+                      <li>
+                          ${Rating.render({
+                              value: product.rating,
+                              text: `${product.numReviews} reviews`,
+                          })}
+                      </li>
+                      <li>
+                          Price: <strong>$${product.price}</strong>
+                      </li>
+                      <li>
+                          Description
+                          <div>
+                              ${product.description}
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+              <div class="details-action">
+                  <ul>
+                      <li>
+                          Price: $${product.price}
+                      </li>
+                      <li>
+                          Status: 
+                              ${
+                                  product.countInStock > 0 
+                              ? `<span class="success">In Stock</span>`
+                              : `<span class="error">Unavailable</span>`
+                              }
+                      </li>
+                      <button id="add-button" class="fw primary">Add To Cart</button>
+                  </ul>
+              </div>
+          </div>
+      </div>`;
        },
    };
 
